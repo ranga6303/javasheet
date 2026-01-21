4a4a41186a8ee81e91470992e3d4865764fc642e
ranga6303 <jangamrangaswamy75@gmail.com>
2026-01-21 07:57:36 +0530

    to trace the leet48

--- src/app/leet48.java
+++ src/app/leet48.java
@@ -10,27 +10,56 @@ class Solution {
     public void rotate(int[][] matrix) {
         int  v1, v2;
         for (int i = 0; i < matrix.length / 2; i++) {
-            for (int j = i; j < matrix.length - i; j++) {
-                
+            System.out.println("iter value:"+i);
+            for (int j = i; j < matrix.length -1-i; j++) {
 
+                if(i==0&&j==0){
+                System.out.println("subitration :"+j);
                 v1 = matrix[i][j];
 
-                v2 = matrix[i][matrix.length - i - 1];
-                matrix[i][matrix.length - i - 1] = v1;
+                v2 = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = v1;
+                Display.array(matrix);
 
                 v1=matrix[matrix.length-1-i][matrix.length-i-1];
-                matrix[matrix.length-1-i][matrix.length-i-1]=v2;
+                matrix[matrix.length-1-i][matrix.length-j-1]=v2;
+                Display.array(matrix);
 
                 v2=matrix[matrix.length-1][j];
                 matrix[matrix.length-1][j]=v1;
+                Display.array(matrix);
 
                 matrix[i][j]=v2;
+                Display.array(matrix);
+                }
 
+
+                else{
+                    System.out.println("subitration :"+j);
+                    v1 = matrix[i][j];
+    
+                    // v2 = matrix[i][matrix.length - j - 1];
+                    // matrix[i][matrix.length - j - 1] = v1;
+                    // Display.array(matrix);
+    
+                    v2=matrix[matrix.length-1-i][matrix.length-i-1];
+                    matrix[matrix.length-1-i][matrix.length-j-1]=v1;
+                    Display.array(matrix);
+    
+                    v1=matrix[matrix.length-1][j];
+                    matrix[matrix.length-1][j]=v2;
+                    Display.array(matrix);
+
+                    v2=matrix[i][j-i];
+    
+                    matrix[i][j]=v2;
+                    Display.array(matrix);
+            }
 
             }
         }
 
-        Display.array(matrix);
+        // Display.array(matrix);
     }
 }
 
